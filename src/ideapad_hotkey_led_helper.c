ab51a4980fbd05bcfa1f1e55fc31d0ece4f4ba3c
Edip Hazuri <edip@medip.dev>
2025-08-21 16:53:22 +0300

    backport to 6.12

--- src/ideapad_hotkey_led_helper.c
+++ src/ideapad_hotkey_led_helper.c
@@ -1,36 +1,150 @@
-// SPDX-License-Identifier: GPL-2.0
-/*
- * Ideapad helper functions for Lenovo Ideapad LED control,
- * It should be included from codec driver.
- */
-
-#if IS_ENABLED(CONFIG_IDEAPAD_LAPTOP)
-
-#include <linux/acpi.h>
-#include <linux/leds.h>
-
-static bool is_ideapad(struct hda_codec *codec)
-{
-	return (codec->core.subsystem_id >> 16 == 0x17aa) &&
-	       (acpi_dev_found("LHK2019") || acpi_dev_found("VPC2004"));
-}
-
-static void hda_fixup_ideapad_acpi(struct hda_codec *codec,
-				   const struct hda_fixup *fix, int action)
-{
-	if (action == HDA_FIXUP_ACT_PRE_PROBE) {
-		if (!is_ideapad(codec))
-			return;
-		snd_hda_gen_add_mute_led_cdev(codec, NULL);
-		snd_hda_gen_add_micmute_led_cdev(codec, NULL);
-	}
-}
-
-#else /* CONFIG_IDEAPAD_LAPTOP */
-
-static void hda_fixup_ideapad_acpi(struct hda_codec *codec,
-				   const struct hda_fixup *fix, int action)
-{
-}
-
-#endif /* CONFIG_IDEAPAD_LAPTOP */
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+<title>kernel/git/stable/linux.git - Linux kernel stable tree</title>
+<meta name='generator' content='cgit 1.2.3-korg'/>
+<meta name='robots' content='noindex, nofollow'/>
+<link rel='stylesheet' type='text/css' href='/cgit-data/cgit.css'/>
+<script type='text/javascript' src='/cgit-data/cgit.js'></script>
+<link rel='shortcut icon' href='/favicon.ico'/>
+<link rel='alternate' title='Atom feed' href='http://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/atom/?h=v6.12' type='application/atom+xml'/>
+<link rel='vcs-git' href='git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git' title='kernel/git/stable/linux.git Git repository'/>
+<link rel='vcs-git' href='https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git' title='kernel/git/stable/linux.git Git repository'/>
+<link rel='vcs-git' href='https://kernel.googlesource.com/pub/scm/linux/kernel/git/stable/linux.git' title='kernel/git/stable/linux.git Git repository'/>
+</head>
+<body>
+<div id='cgit'><table id='header'>
+<tr>
+<td class='logo' rowspan='2'><a href='/'><img src='/cgit-data/cgit.png' alt='cgit logo'/></a></td>
+<td class='main'><a href='/'>index</a> : <a href='/pub/scm/linux/kernel/git/stable/linux.git/'>kernel/git/stable/linux.git</a></td><td class='form'><form method='get'>
+<select name='h' onchange='this.form.submit();'>
+<option value='linux-2.6.11.y'>linux-2.6.11.y</option>
+<option value='linux-2.6.12.y'>linux-2.6.12.y</option>
+<option value='linux-2.6.13.y'>linux-2.6.13.y</option>
+<option value='linux-2.6.14.y'>linux-2.6.14.y</option>
+<option value='linux-2.6.15.y'>linux-2.6.15.y</option>
+<option value='linux-2.6.16.y'>linux-2.6.16.y</option>
+<option value='linux-2.6.17.y'>linux-2.6.17.y</option>
+<option value='linux-2.6.18.y'>linux-2.6.18.y</option>
+<option value='linux-2.6.19.y'>linux-2.6.19.y</option>
+<option value='linux-2.6.20.y'>linux-2.6.20.y</option>
+<option value='linux-2.6.21.y'>linux-2.6.21.y</option>
+<option value='linux-2.6.22.y'>linux-2.6.22.y</option>
+<option value='linux-2.6.23.y'>linux-2.6.23.y</option>
+<option value='linux-2.6.24.y'>linux-2.6.24.y</option>
+<option value='linux-2.6.25.y'>linux-2.6.25.y</option>
+<option value='linux-2.6.26.y'>linux-2.6.26.y</option>
+<option value='linux-2.6.27.y'>linux-2.6.27.y</option>
+<option value='linux-2.6.28.y'>linux-2.6.28.y</option>
+<option value='linux-2.6.29.y'>linux-2.6.29.y</option>
+<option value='linux-2.6.30.y'>linux-2.6.30.y</option>
+<option value='linux-2.6.31.y'>linux-2.6.31.y</option>
+<option value='linux-2.6.32.y'>linux-2.6.32.y</option>
+<option value='linux-2.6.33.y'>linux-2.6.33.y</option>
+<option value='linux-2.6.34.y'>linux-2.6.34.y</option>
+<option value='linux-2.6.35.y'>linux-2.6.35.y</option>
+<option value='linux-2.6.36.y'>linux-2.6.36.y</option>
+<option value='linux-2.6.37.y'>linux-2.6.37.y</option>
+<option value='linux-2.6.38.y'>linux-2.6.38.y</option>
+<option value='linux-2.6.39.y'>linux-2.6.39.y</option>
+<option value='linux-3.0.y'>linux-3.0.y</option>
+<option value='linux-3.1.y'>linux-3.1.y</option>
+<option value='linux-3.10.y'>linux-3.10.y</option>
+<option value='linux-3.11.y'>linux-3.11.y</option>
+<option value='linux-3.12.y'>linux-3.12.y</option>
+<option value='linux-3.13.y'>linux-3.13.y</option>
+<option value='linux-3.14.y'>linux-3.14.y</option>
+<option value='linux-3.15.y'>linux-3.15.y</option>
+<option value='linux-3.16.y'>linux-3.16.y</option>
+<option value='linux-3.17.y'>linux-3.17.y</option>
+<option value='linux-3.18.y'>linux-3.18.y</option>
+<option value='linux-3.19.y'>linux-3.19.y</option>
+<option value='linux-3.2.y'>linux-3.2.y</option>
+<option value='linux-3.3.y'>linux-3.3.y</option>
+<option value='linux-3.4.y'>linux-3.4.y</option>
+<option value='linux-3.5.y'>linux-3.5.y</option>
+<option value='linux-3.6.y'>linux-3.6.y</option>
+<option value='linux-3.7.y'>linux-3.7.y</option>
+<option value='linux-3.8.y'>linux-3.8.y</option>
+<option value='linux-3.9.y'>linux-3.9.y</option>
+<option value='linux-4.0.y'>linux-4.0.y</option>
+<option value='linux-4.1.y'>linux-4.1.y</option>
+<option value='linux-4.10.y'>linux-4.10.y</option>
+<option value='linux-4.11.y'>linux-4.11.y</option>
+<option value='linux-4.12.y'>linux-4.12.y</option>
+<option value='linux-4.13.y'>linux-4.13.y</option>
+<option value='linux-4.14.y'>linux-4.14.y</option>
+<option value='linux-4.15.y'>linux-4.15.y</option>
+<option value='linux-4.16.y'>linux-4.16.y</option>
+<option value='linux-4.17.y'>linux-4.17.y</option>
+<option value='linux-4.18.y'>linux-4.18.y</option>
+<option value='linux-4.19.y'>linux-4.19.y</option>
+<option value='linux-4.2.y'>linux-4.2.y</option>
+<option value='linux-4.20.y'>linux-4.20.y</option>
+<option value='linux-4.3.y'>linux-4.3.y</option>
+<option value='linux-4.4.y'>linux-4.4.y</option>
+<option value='linux-4.5.y'>linux-4.5.y</option>
+<option value='linux-4.6.y'>linux-4.6.y</option>
+<option value='linux-4.7.y'>linux-4.7.y</option>
+<option value='linux-4.8.y'>linux-4.8.y</option>
+<option value='linux-4.9.y'>linux-4.9.y</option>
+<option value='linux-5.0.y'>linux-5.0.y</option>
+<option value='linux-5.1.y'>linux-5.1.y</option>
+<option value='linux-5.10.y'>linux-5.10.y</option>
+<option value='linux-5.11.y'>linux-5.11.y</option>
+<option value='linux-5.12.y'>linux-5.12.y</option>
+<option value='linux-5.13.y'>linux-5.13.y</option>
+<option value='linux-5.14.y'>linux-5.14.y</option>
+<option value='linux-5.15.y'>linux-5.15.y</option>
+<option value='linux-5.16.y'>linux-5.16.y</option>
+<option value='linux-5.17.y'>linux-5.17.y</option>
+<option value='linux-5.18.y'>linux-5.18.y</option>
+<option value='linux-5.19.y'>linux-5.19.y</option>
+<option value='linux-5.2.y'>linux-5.2.y</option>
+<option value='linux-5.3.y'>linux-5.3.y</option>
+<option value='linux-5.4.y'>linux-5.4.y</option>
+<option value='linux-5.5.y'>linux-5.5.y</option>
+<option value='linux-5.6.y'>linux-5.6.y</option>
+<option value='linux-5.7.y'>linux-5.7.y</option>
+<option value='linux-5.8.y'>linux-5.8.y</option>
+<option value='linux-5.9.y'>linux-5.9.y</option>
+<option value='linux-6.0.y'>linux-6.0.y</option>
+<option value='linux-6.1.y'>linux-6.1.y</option>
+<option value='linux-6.10.y'>linux-6.10.y</option>
+<option value='linux-6.11.y'>linux-6.11.y</option>
+<option value='linux-6.12.y'>linux-6.12.y</option>
+<option value='linux-6.13.y'>linux-6.13.y</option>
+<option value='linux-6.14.y'>linux-6.14.y</option>
+<option value='linux-6.15.y'>linux-6.15.y</option>
+<option value='linux-6.16.y'>linux-6.16.y</option>
+<option value='linux-6.2.y'>linux-6.2.y</option>
+<option value='linux-6.3.y'>linux-6.3.y</option>
+<option value='linux-6.4.y'>linux-6.4.y</option>
+<option value='linux-6.5.y'>linux-6.5.y</option>
+<option value='linux-6.6.y'>linux-6.6.y</option>
+<option value='linux-6.7.y'>linux-6.7.y</option>
+<option value='linux-6.8.y'>linux-6.8.y</option>
+<option value='linux-6.9.y'>linux-6.9.y</option>
+<option value='linux-rolling-lts'>linux-rolling-lts</option>
+<option value='linux-rolling-stable'>linux-rolling-stable</option>
+<option value='master'>master</option>
+</select> <input type='submit' value='switch'/></form></td></tr>
+<tr><td class='sub'>Linux kernel stable tree</td><td class='sub right'>Stable Group</td></tr></table>
+<table class='tabs'><tr><td>
+<a href='/pub/scm/linux/kernel/git/stable/linux.git/?h=v6.12'>summary</a><a href='/pub/scm/linux/kernel/git/stable/linux.git/refs/?h=v6.12'>refs</a><a href='/pub/scm/linux/kernel/git/stable/linux.git/log/?h=v6.12'>log</a><a href='/pub/scm/linux/kernel/git/stable/linux.git/tree/?h=v6.12'>tree</a><a href='/pub/scm/linux/kernel/git/stable/linux.git/commit/?h=v6.12'>commit</a><a href='/pub/scm/linux/kernel/git/stable/linux.git/diff/?h=v6.12'>diff</a><a href='/pub/scm/linux/kernel/git/stable/linux.git/stats/?h=v6.12'>stats</a></td><td class='form'><form class='right' method='get' action='/pub/scm/linux/kernel/git/stable/linux.git/log/'>
+<input type='hidden' name='h' value='v6.12'/><select name='qt'>
+<option value='grep'>log msg</option>
+<option value='author'>author</option>
+<option value='committer'>committer</option>
+<option value='range'>range</option>
+</select>
+<input class='txt' type='search' size='10' name='q' value=''/>
+<input type='submit' value='search'/>
+</form>
+</td></tr></table>
+<div class='content'><div class='error'>Not found</div>
+</div> <!-- class=content -->
+<div class='footer'>generated by <a href='https://git.zx2c4.com/cgit/about/'>cgit 1.2.3-korg</a> (<a href='https://git-scm.com/'>git 2.43.0</a>) at 2025-08-21 13:52:15 +0000</div>
+</div> <!-- id=cgit -->
+</body>
+</html>
